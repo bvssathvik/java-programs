2d33636de9680f083b2bddf346ca9f34cff0dc49
bvssathvik <bvssathvik9@gmail.com>
2025-04-07 12:04:55 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Table{
+    int n ;
+public static void main (String[]       args )
+{
+    Scanner read=new Scanner(System.in);
+   System.out.println("enter the value of n");
+   int n = read.nextInt();
+   
+   for  ( Long i=0; i=100000000000l ; i++) {
+       System.out.println(n + "X" +    i+ " = " + (n * i ) );
+   }
+    
+} 
+    
+}
